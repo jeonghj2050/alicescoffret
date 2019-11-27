e3731854baff5c6c71da1b3d597cb41d21a9e6b6
jeonghj <qjrm52@naver.com>
2019-11-27 22:07:41 +0900

    git commit

--- public/javascript/components/product/product_list.js
+++ public/javascript/components/product/product_list.js
@@ -1,17 +1,41 @@
+
+window.$ = window.jQuery = jQuery;
+
 class WriteButton extends React.Component {
-    constructor(props) {
-        super(props);
-    }
     render() {
         var btnStyle = {
             float: 'right',
-            margin:"0 0 20px 0"
+            margin: "0 0 20px 0",
+            color: "white"
         }
-       return(
-           <div>
-               <a href="/sale_post" style={btnStyle} className="btn1">판매글 작성</a>     
-           </div>
-       )
+        console.log("1")
+        return (
+            <div>
+                <a href="/sale_post" style={btnStyle} className="btn1">판매글 작성</a>
+            </div>
+        )
     }
 }
-ReactDOM.render(<WriteButton />, document.getElementById('sale_write_btn'));
+// 클래스명은 대문자만 인식!!
+class TagBox extends React.Component  {
+    render() {
+        console.log("2")
+        var category = ["All", "Earring", "Piercing", "Ring", "Wristband", "Necklace", "Choker", "Clock", "Brooch", "Hairclip"];
+        return (
+            <div>
+                <WriteButton></WriteButton>
+                <div className="tabs_box">
+                    <ul className="tabs">
+                        <li className="active" rel={category[0]} name={category[0]}>전체</li>
+                        <li rel={category[1]} name={category[1]}>귀걸이</li>
+                        <li rel={category[2]} name={category[2]}>피어싱</li>
+                        <li rel={category[3]} name={category[3]}>반지</li>
+                        <li rel={category[4]} name={category[4]}>팔찌</li>
+                    </ul>
+                </div>
+            </div>
+        )
+    }
+}
+
+ReactDOM.render(<TagBox />, document.getElementById('sale_list'));
